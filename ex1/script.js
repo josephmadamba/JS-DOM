ed447c90be42e9798288d019cf49ba179b156761
Joseph Madamba <jpmadamba@gmail.com>
2018-10-25 21:32:11 -0500

    push

--- ex1/script.js
+++ ex1/script.js
@@ -1,1 +1,3 @@
 // Change the text of the h1 tag to "I have changed the text"
+var element = document.getElementById('title')
+element.innerText = 'I have changed the text'
--- ex2/script.js
+++ ex2/script.js
@@ -1,1 +1,3 @@
 // Change the background color of the box to blue
+var boxColor = document.getElementById('box')
+boxColor.style.backgroundColor = 'blue'
